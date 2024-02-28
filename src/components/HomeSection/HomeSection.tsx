613e6aab705c2e3d2dabb53c580fcd5fca4c5e95
Joe Gerhard <jlgerhard@gmail.com>
2024-02-28 10:51:06 -0600

    Update HomeSection

--- src/components/HomeSection/HomeSection.tsx
+++ src/components/HomeSection/HomeSection.tsx
@@ -12,15 +12,15 @@ export default function HomeSection() {
                     Joe Gerhard
                 </h1>
                 <p>
-                    I'm a Frontend Developer with 3+ years of experience
-                    delivering pixel-perfect designs and highly maintainable
-                    code while working in a distributed environment with both
-                    large and small teams. I'm also a former music industry
-                    professional with over 10 years of road-tested experience in
-                    Lighting Design, Audio Engineering, and Tour Management
-                    resulting in a unique creative perspective and a deep
-                    appreciation for connecting with people from all walks of
-                    life.
+                    I'm a frontend developer with a proven track record of over
+                    4 years of translating system requirements into the design
+                    and development of customized systems in an agile
+                    environment. I transitioned from a successful 10-year career
+                    in the music industry, bringing road-tested expertise in
+                    lighting design, audio engineering, and tour management. Now
+                    I'm ready to bring a wealth of technical skills and a rich
+                    creative sensibility to the next level of web development
+                    challenges.
                 </p>
                 <Button $dark onClick={emailMe}>
                     Get in Touch
